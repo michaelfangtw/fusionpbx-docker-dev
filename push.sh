d38b03acc81a549544944623e1804898a972df5a
michael fang <yingchih.fang@gmail.com>
2025-10-19 05:40:20 +0800

    commit to dockerhub

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 TAG=5.2
-docker tag fusionpbx-docker-dev:$TAG michaelfangtw/fusionpbx-docker-dev:$TAG
+docker commit fusionpbx  michaelfangtw/fusionpbx-docker-dev:$TAG
 docker push michaelfangtw/fusionpbx-docker-dev:$TAG
 
